e1a63f7843b5464c39e3f28806dc9ea2ec695d9a
Catalina <cblvallada@gmail.com>
2024-11-10 23:54:39 +0100

    Inicializacion del proyecto con estructura basica (HTML,CSS,JS

---
+++ main.js
@@ -0,0 +1,31 @@
+const apiUrl = 'https://opentdb.com/api.php?amount=10&type=multiple';
+let questions = [];
+let currentQuestionIndex = 0;
+let score = 0;
+let totalAttempts = 0;
+
+let resultsHistory = JSON.parse(localStorage.getItem("quizResults")) || [];
+
+// Muestra la pantalla de bienvenida
+function showWelcomeScreen() {
+    const appContainer = document.getElementById('app');
+    appContainer.innerHTML = `
+        <h1 class="text-center">Bienvenido al Quiz</h1>
+        <p class="text-center">Pulsa "Iniciar" para comenzar el cuestionario</p>
+        <button class="btn btn-primary start" onclick="startQuiz()">Iniciar</button>
+        <div id="chart-container" class="my-4">
+            <canvas id="resultChart"></canvas>
+        </div>
+    `;
+    renderChart();
+}
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    totalAttempts = 0;
+    fetchQuestions();
+}
+
+//**Commit**: `Agrega función de pantalla de bienvenida y botón de inicio`
+
